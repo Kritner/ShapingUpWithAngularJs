ab27849a4e91e80a011b6736624b07eecd082d22
Russ Hammett <Kritner@gmail.com>
2015-12-13 14:15:48 -0500

    Added angular and tested that an expression works on the "gemStore" app.

---
+++ src/ShapingUpWithAngularJs/Controllers/GemStoreController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ShapingUpWithAngularJs.GemStore.Controllers
+{
+    public class GemStoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
