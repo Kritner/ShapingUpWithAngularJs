0d2d895b0be0f15a0b525597fa57b022072b7546
Russ Hammett <Kritner@gmail.com>
2015-12-13 15:16:32 -0500

    1.1 - 1.5

--- src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
+++ src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
@@ -1,1 +1,25 @@
-﻿var app = angular.module('gemStore', [ ]);
+﻿(function () {
+
+    var app = angular.module('gemStore', []);
+
+    app.controller('StoreController', function () {
+        this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecohedron',
+            price: 2.95,
+            description: 'Oh man this gem is SICK!',
+            canPurchase: true,
+            soldOut: true
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: 'This gem looks like the pentagon!',
+            canPurchase: false
+        }
+    ];
+
+})();
