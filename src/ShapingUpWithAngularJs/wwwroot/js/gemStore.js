9e03ffebc3087e8144667e9d6c4b61891d8e2333
Russ Hammett <Kritner@gmail.com>
2015-12-13 16:37:12 -0500

    finished section 2

--- src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
+++ src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
@@ -1,9 +1,21 @@
 ﻿(function () {
 
-    var app = angular.module('gemStore', []);
+    var app = angular.module("gemStore", []);
 
-    app.controller('StoreController', function () {
+    app.controller("StoreController", function () {
         this.products = gems;
+    });
+
+    app.controller("PanelController", function () {
+        this.tab = 1;
+
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
     });
 
     var gems = [
