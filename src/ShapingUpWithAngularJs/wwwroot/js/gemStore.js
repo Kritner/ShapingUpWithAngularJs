98f4accea3b0eaf321ede948543de5d1f85e5eae
Russ Hammett <Kritner@gmail.com>
2015-12-13 19:11:31 -0500

    Pre adding directive for ProductPanels

--- src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
+++ src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
@@ -27,6 +27,20 @@
         };
     });
 
+    app.directive('productTitle', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'product-panels.html'
+        };
+    });
+
     var gems = [
         {
             name: 'Dodecohedron',
