a0d2d029c46cb28cd855d51a45b0eb1294e39d2f
Russ Hammett <Kritner@gmail.com>
2015-12-13 19:50:47 -0500

    level 4

--- src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
+++ src/ShapingUpWithAngularJs/wwwroot/js/gemStore.js
@@ -6,18 +6,6 @@
         this.products = gems;
     });
 
-    app.controller("PanelController", function () {
-        this.tab = 1;
-
-        this.selectTab = function (setTab) {
-            this.tab = setTab;
-        };
-
-        this.isSelected = function (checkTab) {
-            return this.tab === checkTab;
-        };
-    });
-
     app.controller("ReviewController", function () {
         this.review = {};
 
@@ -30,14 +18,26 @@
     app.directive('productTitle', function () {
         return {
             restrict: 'E',
-            templateUrl: 'product-title.html'
+            templateUrl: 'GemStore/product-title.html'
         };
     });
 
     app.directive('productPanels', function () {
         return {
             restrict: 'E',
-            templateUrl: 'product-panels.html'
+            templateUrl: 'GemStore/product-panels.html',
+            controller: function () {
+                this.tab = 1;
+
+                this.selectTab = function (setTab) {
+                    this.tab = setTab;
+                };
+
+                this.isSelected = function (checkTab) {
+                    return this.tab === checkTab;
+                };
+            },
+            controllerAs: 'panel'
         };
     });
 
